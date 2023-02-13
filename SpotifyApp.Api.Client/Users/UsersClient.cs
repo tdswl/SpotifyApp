1510a6a0bfaad05bc5423a2798a8bd52be784d90
twl <todayswl@proton.me>
2023-02-13 11:07:00 +0300

    Add all users api methods

--- SpotifyApp.Api.Client/Users/UsersClient.cs
+++ SpotifyApp.Api.Client/Users/UsersClient.cs
@@ -1,4 +1,3 @@
-using Flurl;
 using SpotifyApp.Api.Contracts.Users.Responses;
 using Flurl.Http;
 using SpotifyApp.Api.Contracts.Users.Requests;
@@ -41,6 +40,39 @@ internal class UsersClient : IUsersClient
         return query.GetJsonAsync<GetUsersTopItemsResponse>(cancellationToken);
     }
 
+    Task<GetUsersProfileResponse> IUsersClient.GetUsersProfile(string userId, 
+        string accessToken, 
+        CancellationToken cancellationToken)
+    {
+        return "https://api.spotify.com/v1/users"
+            .WithOAuthBearerToken(accessToken)
+            .AppendPathSegment(userId)
+            .GetJsonAsync<GetUsersProfileResponse>(cancellationToken);
+    }
+
+    Task IUsersClient.FollowPlaylist(string playlistId, 
+        FollowPlaylistRequest request,
+        string accessToken, 
+        CancellationToken cancellationToken)
+    {
+        return "https://api.spotify.com/v1/playlists"
+            .WithOAuthBearerToken(accessToken)
+            .AppendPathSegment(playlistId)
+            .AppendPathSegment("followers")
+            .PutJsonAsync(request, cancellationToken: cancellationToken);
+    }
+
+    Task IUsersClient.UnfollowPlaylist(string playlistId, 
+        string accessToken, 
+        CancellationToken cancellationToken)
+    {
+        return "https://api.spotify.com/v1/playlists"
+            .WithOAuthBearerToken(accessToken)
+            .AppendPathSegment(playlistId)
+            .AppendPathSegment("followers")
+            .DeleteAsync(cancellationToken: cancellationToken);
+    }
+    
     Task<GetFollowedArtistsResponse> IUsersClient.GetFollowedArtists(GetFollowedArtistsRequest request, 
         string accessToken, 
         CancellationToken cancellationToken)
@@ -60,5 +92,51 @@ internal class UsersClient : IUsersClient
         }
         
         return query.GetJsonAsync<GetFollowedArtistsResponse>(cancellationToken);
+    }
+
+    Task IUsersClient.FollowArtistsOrUsers(FollowArtistsOrUsersRequest request, 
+        string accessToken, 
+        CancellationToken cancellationToken)
+    {
+        return "https://api.spotify.com/v1/me/following"
+            .WithOAuthBearerToken(accessToken)
+            .SetQueryParam("type", request.Type.ToString().ToLower())
+            .SetQueryParam("ids", request.Ids)
+            .PutAsync(cancellationToken: cancellationToken);
+    }
+
+    Task IUsersClient.UnfollowArtistsOrUsers(FollowArtistsOrUsersRequest request, 
+        string accessToken,
+        CancellationToken cancellationToken)
+    {
+        return "https://api.spotify.com/v1/me/following"
+            .WithOAuthBearerToken(accessToken)
+            .SetQueryParam("type", request.Type.ToString().ToLower())
+            .SetQueryParam("ids", request.Ids)
+            .DeleteAsync(cancellationToken);
+    }
+
+    Task<IReadOnlyList<bool>> IUsersClient.CheckIfUserFollowsArtistsOrUsers(FollowArtistsOrUsersRequest request,
+        string accessToken,
+        CancellationToken cancellationToken)
+    {
+        return "https://api.spotify.com/v1/me/following/contains"
+            .WithOAuthBearerToken(accessToken)
+            .SetQueryParam("type", request.Type.ToString().ToLower())
+            .SetQueryParam("ids", request.Ids)
+            .GetJsonAsync<IReadOnlyList<bool>>(cancellationToken);
+    }
+
+    Task<IReadOnlyList<bool>> IUsersClient.CheckIfUsersFollowPlaylist(string playlistId, 
+        string ids, 
+        string accessToken, 
+        CancellationToken cancellationToken)
+    {
+        return "https://api.spotify.com/v1/playlists"
+            .WithOAuthBearerToken(accessToken)
+            .AppendPathSegment(playlistId)
+            .AppendPathSegment(playlistId)
+            .SetQueryParam("followers/contains")
+            .GetJsonAsync<IReadOnlyList<bool>>(cancellationToken);
     }
 }

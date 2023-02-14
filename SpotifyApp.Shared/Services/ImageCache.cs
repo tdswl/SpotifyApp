ce86774b1df82c976d53c635b65b6741cf27018c
twl <todayswl@proton.me>
2023-02-14 23:13:18 +0300

    Add lock to prevent download same image

--- SpotifyApp.Shared/Services/ImageCache.cs
+++ SpotifyApp.Shared/Services/ImageCache.cs
@@ -1,3 +1,4 @@
+using AsyncKeyedLock;
 using Flurl.Http;
 
 namespace SpotifyApp.Shared.Services;
@@ -5,23 +6,27 @@ namespace SpotifyApp.Shared.Services;
 internal class ImageCache : IImageCache 
 {
     private const string CacheFolder = "Cache";
+    private static readonly AsyncKeyedLocker<string> AsyncKeyedLocker = new();
     
     public async Task<string> GetImage(string webPath, CancellationToken cancellationToken)
     {
-        if (!Directory.Exists(CacheFolder))
+        using (await AsyncKeyedLocker.LockAsync(webPath, cancellationToken))
         {
-            Directory.CreateDirectory(CacheFolder);
-        }
-        
-        var fileName = Path.GetFileName(new Uri(webPath).AbsolutePath);
+            if (!Directory.Exists(CacheFolder))
+            {
+                Directory.CreateDirectory(CacheFolder);
+            }
 
-        var filePath = $"{CacheFolder}/{fileName}";
-        if (File.Exists(filePath))
-        {
-            return filePath;
+            var fileName = Path.GetFileName(new Uri(webPath).AbsolutePath);
+
+            var filePath = $"{CacheFolder}/{fileName}";
+            if (File.Exists(filePath))
+            {
+                return filePath;
+            }
+
+            return await webPath
+                .DownloadFileAsync(CacheFolder, fileName, cancellationToken: cancellationToken);
         }
-        
-        return await webPath
-            .DownloadFileAsync(CacheFolder, fileName, cancellationToken: cancellationToken);
     }
 }

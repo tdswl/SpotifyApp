53b5819146c194a59bcc2dfa9a59ce037363bad5
tdswl <todayswl@proton.me>
2023-02-15 22:00:02 +0300

    Add lock to auth service

--- SpotifyApp.Shared/Services/IAuthService.cs
+++ SpotifyApp.Shared/Services/IAuthService.cs
@@ -5,6 +5,4 @@ namespace SpotifyApp.Shared.Services;
 public interface IAuthService
 {
     public Task<AuthorizationInfoModel> Login(CancellationToken token);
-
-    public Task<AuthorizationInfoModel> RefreshToken(string refreshToken, CancellationToken token);
 }

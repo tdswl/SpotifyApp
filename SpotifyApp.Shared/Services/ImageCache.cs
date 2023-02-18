a73f1da56d17ad2e343bb22dd35ca1dada4aa90d
tdswl <26434540+tdswl@users.noreply.github.com>
2023-02-18 22:18:08 +0300

    Switch to System.Text.Json, remove Flurl and Newtonsoft

--- SpotifyApp.Shared/Services/ImageCache.cs
+++ SpotifyApp.Shared/Services/ImageCache.cs
@@ -1,16 +1,21 @@
 using AsyncKeyedLock;
-using Flurl.Http;
 
 namespace SpotifyApp.Shared.Services;
 
 internal class ImageCache : IImageCache 
 {
     private const string CacheFolder = "Cache";
+    private readonly HttpClient _httpClient;
     private static readonly AsyncKeyedLocker<string> AsyncKeyedLocker = new(o =>
     {
         o.PoolSize = 20;
         o.PoolInitialFill = 1;
     });
+
+    public ImageCache(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+    }
     
     public async Task<string> GetCachedImagePath(string webPath, CancellationToken cancellationToken)
     {
@@ -29,8 +34,15 @@ internal class ImageCache : IImageCache
                 return filePath;
             }
 
-            return await webPath
-                .DownloadFileAsync(CacheFolder, fileName, cancellationToken: cancellationToken);
+            await using (var webStream = await _httpClient.GetStreamAsync(webPath, cancellationToken))
+            {
+                await using (var fileStream = File.OpenWrite(filePath))
+                {
+                    await webStream.CopyToAsync(fileStream, cancellationToken);
+                }
+            }
+
+            return filePath;
         }
     }
 }
